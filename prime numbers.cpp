877befcc3de3ae75ccf0eb88f9655bb746194c2b
learner959 <tiwariankur2007@gmail.com>
2025-11-11 17:43:53 +0530

    Add files via upload

---
+++ prime numbers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int a;
+
+    cin>>a;
+
+    for (int i=2;i<a;i++){
+        if(a%i==0){
+            cout<<"given number is not a prime number";
+        }
+    }
+}
